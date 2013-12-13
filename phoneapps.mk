d1f61f987e9390bdbe1883fc1e12aff015d758e1
jiangjiawen <jwjiang@lewatek.com>
2013-12-13 14:23:45 +0800

    upload patchrom base on android 4.2.2

---
+++ phoneapps.mk
@@ -0,0 +1,17 @@
+local-phone-apps = $(private-phone-apps)
+
+private-phone-apps := ApplicationsProvider \
+					  BackupRestoreConfirmation \
+					  CertInstaller \
+					  DefaultContainerService \
+					  KeyChain \
+					  MusicFX \
+					  Stk \
+					  UserDictionaryProvider \
+					  VpnDialogs \
+					  WAPPushManager \
+					  Gallery2 \
+					  UserDictionaryProvider \
+					  Email \
+					  LatinIME \
+					  Phone
